912a4c3b7dc4ced4efb7976d9392cc1f457e5400
AzureAD\MDSALEHZOHURBHUIYAN <saleh@erainfotechbd.com>
2025-05-05 11:36:31 +0600

    prepare command chaged

--- projects/component-lib-2/.storybook/main.ts
+++ projects/component-lib-2/.storybook/main.ts
@@ -1,18 +1,15 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/angular",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
 };
 export default config;
