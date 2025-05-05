1a70655ca20a3c613fb3e82be0f1207c4fac5c0f
saleh-zo-niloy <saleh@erainfotechbd.com>
2025-05-05 16:00:21 +0600

    Delete dist/component-lib-2 directory

--- dist/component-lib-2/lib/components/button/button.component.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class ButtonComponent {
-    static ɵfac: i0.ɵɵFactoryDeclaration<ButtonComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<ButtonComponent, "lib-btn", never, {}, {}, never, never, true, never>;
-}
-//# sourceMappingURL=button.component.d.ts.map
